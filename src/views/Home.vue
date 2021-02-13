4df1c62c0b02f8bb444b86405e86dd8f10364578
Ste Riley <ste@steriley.com>
2021-02-13 20:07:43 +0000

    initial graphing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,87 @@
 <template>
-  <div class="home">
-    <Graph v-bind="{ data }" />
+  <div class="home" v-if="json">
+    <vue-frappe
+      id="temperature"
+      :labels="labels"
+      title="Temperature"
+      :height="300"
+      :dataSets="temperature.datasets"
+      :tooltipOptions="temperature.tooltipOptions"
+    />
+    <vue-frappe
+      id="humidity"
+      :labels="labels"
+      title="Humidity"
+      :height="300"
+      :dataSets="humidity.datasets"
+      :tooltipOptions="humidity.tooltipOptions"
+    />
+    <vue-frappe
+      id="pressure"
+      :labels="labels"
+      title="Pressure"
+      :height="300"
+      :dataSets="pressure.datasets"
+      :tooltipOptions="pressure.tooltipOptions"
+    />
   </div>
 </template>
 
 <script>
-import Graph from '@/components/Graph.vue';
+import { VueFrappe } from 'vue2-frappe';
 
 export default {
-  data: () => ({
-    data: null,
-    room: 'boxroom',
-  }),
   components: {
-    Graph,
+    VueFrappe,
   },
+  data: () => ({
+    labels: [],
+    temperature: {
+      datasets: [],
+      tooltipOptions: {
+        formatTooltipY: (d) => `${d}C`,
+      },
+    },
+    humidity: {
+      datasets: [],
+      tooltipOptions: {
+        formatTooltipY: (d) => `${d}%`,
+      },
+    },
+    pressure: {
+      datasets: [],
+      tooltipOptions: {
+        formatTooltipY: (d) => `${d}Bar`,
+      },
+    },
+    room: 'boxroom',
+    json: null,
+  }),
   async mounted() {
-    const json = await fetch(`./${this.room}.json`).then((data) => data.json());
+    this.json = await fetch(`./${this.room}.json`).then((data) => data.json());
 
-    this.data = json;
+    this.labels = this.json.map((record) => {
+      const d = new Date(record.d);
+      return `${d.getDate()}-${d.getMonth() + 1} ${d.getHours()}:${d.getMinutes()}`;
+    });
+
+    this.temperature.datasets.push({
+      name: 'Temperature',
+      chartType: 'line',
+      values: this.json.map((x) => x.t),
+    });
+
+    this.humidity.datasets.push({
+      name: 'Humidity',
+      chartType: 'bar',
+      values: this.json.map((x) => x.h),
+    });
+
+    this.pressure.datasets.push({
+      name: 'Pressure',
+      chartType: 'line',
+      values: this.json.map((x) => x.p),
+    });
   },
 };
 </script>
